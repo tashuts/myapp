8779158d2ec1b4406f9beee0720d0458099bcbff
tashuts <165865596+tashuts@users.noreply.github.com>
2024-12-09 23:24:01 -0800

    added webhook method

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 import git
+import subprocess
 
 # Create your views here.
 def home(request):
@@ -9,4 +10,8 @@ def home(request):
 def update_webhook(request):
     # function to handle webhook push from github
     # one more comment added
-    pass
+    repo = git.Repo('/home/tanishashankpal/myprofile/myapp')
+    repo.remotes.origin.pull()
+    # python manage.py collectstatic --noinput
+    subprocess.run(["cd", "/home/tanishashankpal/myprofile"])
+    subprocess.run(["python", "manage.py", "collectstatic", "--noinput"])
