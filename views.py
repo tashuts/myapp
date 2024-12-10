8823474dfe513dbfb03c420ee8347ba6ea927b69
tashuts <165865596+tashuts@users.noreply.github.com>
2024-12-09 22:32:49 -0800

    to test git pull from pythonanywhere

--- views.py
+++ views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, HttpResponse
+import git
 
 # Create your views here.
 def home(request):
     # return HttpResponse("Hello World!")
     return render(request, 'base.html')
+
+def update_webhook(request):
+    # function to handle webhook push from github
+    pass
